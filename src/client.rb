0dfdc8db56176cc8f2628fc77c9b95d0ec56277d
Haruka Matsumoto <harukam0416@gmail.com>
2019-09-28 23:17:12 +0900

    Add client script

---
+++ src/client.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+def main()
+  if ARGV.size != 1
+    raise "Not arg~"
+  end
+
+  socket = UDPSocket.new
+  socket.send(ARGV[0], 0, 'localhost', 8098)
+  socket.close
+
+  puts "Just sent ^-^"
+end
+
+main
